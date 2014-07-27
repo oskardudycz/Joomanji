727c6d28c5b4e9979d04520a33cf8d6bcd02c6ee
oskar.dudycz@gmail.com <oskar.dudycz@gmail.com>
2014-07-27 21:27:09 +0200

    added missing files

---
+++ src/Shared.Core.System.NET45/TypeForwarding.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(IValidatableObject))]
+[assembly: TypeForwardedTo(typeof(ValidationContext))]
+[assembly: TypeForwardedTo(typeof(ValidationResult))]
+[assembly: TypeForwardedTo(typeof(SerializableAttribute))]
